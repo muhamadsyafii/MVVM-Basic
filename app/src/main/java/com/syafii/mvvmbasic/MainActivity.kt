7de64242ca1ffd6abbc53cf40bfec5a620af7e6c
Muhamad Syafii <muhamadsyafii@Syafii-MacBook-Pro.local>
2021-06-06 12:38:08 +0700

    add some depedency in build gradle

--- app/src/main/java/com/syafii/mvvmbasic/MainActivity.kt
+++ app/src/main/java/com/syafii/mvvmbasic/MainActivity.kt
@@ -2,6 +2,7 @@ package com.syafii.mvvmbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewbinding.BuildConfig
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/syafii/mvvmbasic/utils/Constants.kt
@@ -0,0 +1,3 @@
+package com.syafii.mvvmbasic.utils
+
+const val BASE_URL = ""
